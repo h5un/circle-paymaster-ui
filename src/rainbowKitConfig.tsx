649738accf8daa11c160caa8904e07b30a2e31f4
h5un <wupinghsun.tw@gmail.com>
2025-07-05 12:48:34 +0800

    chore: update rainbowKitConfig to disable SSR and fix WalletConnect client-side issue

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -8,11 +8,14 @@ if (!walletConnectProjectId) {
     throw new Error("Error: NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID is not defined. Please set it in your .env.local file");
 }
 
-const config = getDefaultConfig({
-    appName: "circle-paymaster", // Your dApp's name, shown in wallet prompts
-    projectId: walletConnectProjectId, // WalletConnect Cloud Project ID
-    chains: [anvil, arbitrumSepolia], // Array of chains your dApp supports
-    ssr: false, // Set to false for static sites or if not heavily using SSR with wagmi
-});
-  
-export default config;
+let config: ReturnType<typeof getDefaultConfig>;
+// 防止在 SSR 過程中觸發 WalletConnect 初始化
+if (typeof window !== "undefined") {
+    config = getDefaultConfig({
+        appName: "circle-paymaster", // Your dApp's name, shown in wallet prompts
+        projectId: walletConnectProjectId, // WalletConnect Cloud Project ID
+        chains: [anvil, arbitrumSepolia], // Array of chains your dApp supports
+        ssr: false, // Set to false for static sites or if not heavily using SSR with wagmi
+    });
+}
+export default config!;
